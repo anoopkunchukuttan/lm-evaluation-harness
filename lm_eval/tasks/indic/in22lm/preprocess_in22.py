06fd06ccbedc13926b555446611135ded352ccfb
Anoop Kunchukuttan <anoop.kunchukuttan@gmail.com>
2025-11-11 08:25:27 +0000

    fix: in22lm

--- lm_eval/tasks/indic/in22lm/preprocess_in22.py
+++ lm_eval/tasks/indic/in22lm/preprocess_in22.py
@@ -1,12 +1,21 @@
 import re
 
-def process_results(doc, results):
-    (loglikelihood,) = results
-    # IMPORTANT: wikitext counts number of words in *original doc before detokenization*
-    _words = len(re.split(r"\s+", doc["page"]))
-    _bytes = len(doc["page"].encode("utf-8"))
-    return {
-        "word_perplexity": (loglikelihood, _words),
-        "byte_perplexity": (loglikelihood, _bytes),
-        "bits_per_byte": (loglikelihood, _bytes),
-    }
+def create_process_results(language_code):
+    """Factory function that creates a process_results function for a specific language"""
+    def process_results(doc, results):
+        (loglikelihood,) = results
+        # language_code is available here via closure
+        field_name = f"{language_code}"  # e.g., "hin_Deva"
+        
+        _words = len(re.split(r"\s+", doc[field_name]))
+        _bytes = len(doc[field_name].encode("utf-8"))
+        
+        return {
+            "word_perplexity": (loglikelihood, _words),
+            "byte_perplexity": (loglikelihood, _bytes),
+            "bits_per_byte": (loglikelihood, _bytes),
+        }
+    return process_results
+
+# Create specific functions
+process_results_hin_Deva = create_process_results("hin_Deva")
