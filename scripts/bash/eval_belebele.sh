9b20464b248b5033ab6f67a5cd59c5de8590cb1e
Yellala Vishnu Vardhan Reddy <y.vishnu55@gmail.com>
2025-12-03 18:27:22 +0530

    Fix the use accelerate issue

--- scripts/bash/eval_belebele.sh
+++ scripts/bash/eval_belebele.sh
@@ -46,7 +46,7 @@ do
 	model_str=$(echo "$model" | sed 's#/#__#g')
 
 	accelerate launch --multi_gpu -m lm_eval --model hf \
-		--model_args pretrained=$model,dtype="$dtype",trust_remote_code=True,device_map=balanced,use_accelerate=True \
+		--model_args pretrained=$model,dtype="$dtype",trust_remote_code=True,device_map=balanced \
 		--tasks $task_str	\
 		--batch_size 4 \
 		--output_path $RESDIR \
